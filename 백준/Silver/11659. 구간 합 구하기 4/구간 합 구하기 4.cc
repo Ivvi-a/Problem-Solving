9b956ab15038d0baf7ebab2d7bd2d496868cef05
Ivvi <53993041+Y-Pearl-Jo@users.noreply.github.com>
2023-11-07 22:34:49 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 40 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -3,26 +3,24 @@ using namespace std;
 
 int main()
 {
-    ios_base::sync_with_stdio(false); // c++ 입출력 버퍼와 c 입출력 버퍼 동기화 xor
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
     cout.tie(NULL);
-    cin.tie(NULL); // 입출력 스트림의 버퍼와 연결 끊기
-
-    int N,M;
     
+    int N,M;
     cin >> N >> M;
-    int list[N+1];
-    list[0] = 0;
-    for(int i=1; i<N+1; i++){
-        int n;
-        cin >> n;
-        list[i] = list[i-1]+n;
+    
+    int sum[100001];
+    for(int i=1; i<=N; i++){
+        int temp;
+        cin >> temp;
+        sum[i] = sum[i-1] + temp;
     }
     
     for(int i=0; i<M; i++){
-        int s,e;
-        cin >> s >> e;
-        cout<< list[e] - list[s-1] << "\n";
+        int start, end;
+        cin >> start >> end;
+        cout << sum[end] - sum[start-1] << "\n";
     }
     
-    return 0;
 }
