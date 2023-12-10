5d95b27715926670ac37d1f69d6916943b27803e
Ivvi <53993041+Y-Pearl-Jo@users.noreply.github.com>
2023-12-10 18:26:27 +0900

    [Silver I] Title: 본대 산책, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/12849. 본대 산책/본대 산책.cc
+++ 백준/Silver/12849. 본대 산책/본대 산책.cc
@@ -1,67 +1,36 @@
+// dp
+
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
-int arr[8][8] = {
-	{0,1,1,0,0,0,0,0}, // 0
-	{1,0,1,1,0,0,0,0}, // 1
-	{1,1,0,1,1,0,0,0}, // 2
-	{0,1,1,0,1,1,0,0}, // 3
-	{0,0,1,1,0,1,1,0}, // 4
-	{0,0,0,1,1,0,0,1}, // 5
-	{0,0,0,0,1,0,0,1}, // 6
-	{0,0,0,0,0,1,1,0} // 7
-};
-
-int ans[8][8] = {
-	{0,1,1,0,0,0,0,0}, // 0
-	{1,0,1,1,0,0,0,0}, // 1
-	{1,1,0,1,1,0,0,0}, // 2
-	{0,1,1,0,1,1,0,0}, // 3
-	{0,0,1,1,0,1,1,0}, // 4
-	{0,0,0,1,1,0,0,1}, // 5
-	{0,0,0,0,1,0,0,1}, // 6
-	{0,0,0,0,0,1,1,0} // 7
-};
-
-int temp[8][8];
+long long dp[8], temp[8];
 
 int dd = 1000000007;
-
-void sqr(int r, int c);
 
 int main()
 {
 	int D;
-	cin >> D; // 제곱할 횟수
+	cin >> D; 
+
+	dp[0] = 1;
 	
-	while (D-- > 1) {
-		// 제곱
-		for (int r = 0; r < 8; r++) {
-			for (int c = 0; c < 8; c++) {
-				sqr(r,c);
-			}
-		}
-		// 복사 temp -> ans
-		for (int r = 0; r < 8; r++) {
-			for (int c = 0; c < 8; c++) {
-				ans[r][c] = temp[r][c];
-			}
+	while (D--) {
+		temp[0] = dp[1] + dp[2];
+		temp[1] = dp[0] + dp[2] + dp[3];
+		temp[2] = dp[0] + dp[1] + dp[3] + dp[4];
+		temp[3] = dp[1] + dp[2] + dp[4] + dp[5];
+		temp[4] = dp[2] + dp[3] + dp[5] + dp[6];
+		temp[5] = dp[3] + dp[4] + dp[7];
+		temp[6] = dp[4] + dp[7];
+		temp[7] = dp[5] + dp[6];
+
+		for (int i = 0; i < 8; i++) {
+			dp[i] = temp[i] % dd;
 		}
 	}
 
 	// 출력
-	cout << ans[0][0];
-
-}
-
-void sqr(int r, int c) {
-	long long n = 0;
-
-	for (int i = 0; i < 8; i++) {
-		n += (static_cast<long long>(ans[r][i] % dd) * (arr[i][c]%dd)) % dd;
-	}
-
-	temp[r][c] = n % dd;
+	cout << dp[0];
 
 }
