e091cef513f6d1bc5d8cc818b0a980684326c10c
Ivvi <53993041+Y-Pearl-Jo@users.noreply.github.com>
2024-01-04 23:55:09 +0900

    Update 가장 긴 증가하는 부분 수열 5.cc

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -1,76 +1,84 @@
-#include <iostream>
-#include <memory.h>
-#include <vector>
-#include <algorithm>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
-int A[1000000], V[1000000];
 
-int main(void)
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
+int *A, *idx;
+vector<int> v;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
 	int N;
-	cin >> N;
+	cin >> N; // 수열 A의 크기
+	A = new int[N];
+	idx = new int[N];
 
-	for (int i = 0; i < N; ++i)
-	{
+	// 수열 입력받기
+	for (int i = 0; i < N; i++) {
 		cin >> A[i];
 	}
 
-	vector<int> vec; 
-	vec.push_back(A[0]);
-    memset(V, -1, sizeof(V));
-	V[0] = 0;
-	for (int i = 1; i < N; ++i) {
-		if (vec.back() < A[i])
-		{
-			vec.push_back(A[i]);
-			V[i] = vec.size() - 1;
+	// 첫번째 값 넣기
+	v.push_back(A[0]);
+	idx[0] = 0;
+
+	// 두번째 값 ~ 끝까지
+	for (int i = 1; i < N; i++) {
+		int now = A[i];
+
+		// 현재 값이 백터의 마지막 값보다 큰 경우
+		if (v.back() < now) {
+			v.push_back(now);
+			idx[i] = v.size() - 1;
 		}
-		else
-		{
-			int start = 0; 
-			int end = vec.size() - 1;
-			int mid = end;
+		// 아닌 경우, 중간에 끼워넣어야 함(치환) -> 이분 탐색
+		else {
+			int start = 0;
+			int end = v.size() - 1;
 
-			while (start < end)
-			{
-				mid = (start + end) / 2; // (start = 0 + end = 3)  2 = 1 
-
-				if (vec[mid] < A[i]) // ex) vec[] = 10,(*)15, 18, 25, 30 <--- push (20)
-				{
+			while (start < end) {
+				int mid = (start + end) / 2;
+				if (v[mid] < now) {
 					start = mid + 1;
 				}
-				else // ex) vec[] = 10, 15, 18,(*)25 30 <--- push (20)
-				{
+				else {
 					end = mid;
 				}
-
 			}
-            vec[end] = A[i];
-			V[i] = end;
+
+			idx[i] = end;
+			v[end] = A[i];
 		}
 	}
-	//=======================================//
-	vector<int> _result;
-	int index = vec.size() - 1;
 
-	for (int i = N; i >= 0; --i)
-	{
-		if (V[i] == index)
-		{
-			_result.push_back(A[i]);
-			--index;
+	// LIS 찾기 : 역순으로 찾은 후 뒤집기
+	vector<int> LIS;
+	int indexx = v.size() - 1;
+
+	for (int i = N-1; i >= 0; i--) {
+		if (indexx == idx[i]) {
+			LIS.push_back(A[i]);
+			indexx--;
+			// 종료 조건
+			if (indexx == -1) {
+				break;
+			}
 		}
 	}
-	//=======================================//
-	cout << vec.size() << "\n";
 
-	reverse(_result.begin(), _result.end());
+	reverse(LIS.begin(), LIS.end());
 
-	for (auto& res : _result)
-	{
-		cout << res << " ";
+	// LIS 길이 출력
+	cout << v.size() << "\n";
+
+	// LIS 출력
+	for (int i = 0; i < LIS.size(); i++) {
+		cout << LIS[i] << " ";
 	}
+
+	delete[] A;
+	delete[] idx;
+
 	return 0;
 }
