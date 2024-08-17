b46a4498b1d72af970abb24345cecef3cd959a07
Ivvi <53993041+Ivvi-a@users.noreply.github.com>
2024-08-17 20:17:27 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14296 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,26 +1,28 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int MOD = 1234567891;
+    static BigInteger MOD = new BigInteger("1234567891");
 
     public static void main(String[] args) throws IOException {
         int L = input();
         String str = br.readLine();
 
-        long ans = 0;
+        BigInteger ans = new BigInteger("0");
+        BigInteger hash = new BigInteger("1");
+
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-            ans += (long) ((c-96) * (Math.pow(31,i)%MOD));
+            ans = ans.add(BigInteger.valueOf(c-96).multiply(hash));
+            ans = ans.mod(MOD);
+            hash = hash.multiply(BigInteger.valueOf(31));
         }
-
-        ans %= MOD;
 
         System.out.println(ans);
 
     }
-
 
     // 읽기
     static int input() throws IOException {
