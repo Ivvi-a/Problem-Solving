a65826eb2aec9d20d4aee65ce0eb71c2bb0618f8
Ivvi <53993041+Ivvi-a@users.noreply.github.com>
2024-10-10 15:41:19 +0900

    [Silver V] Title: 분수찾기, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int[] dots = new int[16];
 
     public static void main(String[] args) throws IOException {
         int X = Integer.parseInt(br.readLine());
