3b556891f5c5be40308aabaed5a5679372dafcc5
Yeonju Jo <53993041+Ivvi-a@users.noreply.github.com>
2024-11-25 00:40:53 +0900

    [Platinum IV] Title: 어려운 계단 수, Time: 96 ms, Memory: 127144 KB -BaekjoonHub

---
+++ 백준/Platinum/18015. 어려운 계단 수/어려운 계단 수.cc
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<cstring>
+#define MOD 1000000000
+using namespace std;
+typedef long long ll;
+ll dp[2001][2001][2][2];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int N,B;
+    cin >> N >> B;
+
+    if (N < B) {
+        cout << 0;
+        return 0;
+    }
+
+    // dp[길이][마지막자릿수][0 존재여부][B-1 존재여부]
+    memset(dp, 0LL, sizeof dp);
+
+    dp[1][B-1][0][1] = 1;
+    for (int i = 1; i < B-1; i++) {
+        dp[1][i][0][0] = 1;
+    }
+
+    for (int i = 2; i <= N; i++) {
+        for (int k = 0; k < B; k++) {
+            if (k == 0) {
+                for (int b = 0; b <= 1; b++) {
+                    dp[i][k][1][b] = (dp[i][k][1][b] + dp[i - 1][k + 1][1][b] + dp[i - 1][k + 1][0][b]) % MOD;
+                }
+            }
+            else if (k == B - 1) {
+                for (int z = 0; z <= 1; z++) {
+                    dp[i][k][z][1] = (dp[i][k][z][1] + dp[i - 1][k - 1][z][1] + dp[i - 1][k - 1][z][0]) % MOD;
+
+                }
+            }
+            else {
+                for (int b = 0; b <= 1; b++) {
+                    for (int z = 0; z <= 1; z++) {
+                        dp[i][k][z][b] = (dp[i][k][z][b] + dp[i - 1][k - 1][z][b] + dp[i - 1][k + 1][z][b]) % MOD;
+                    }
+                }
+            }
+        }
+    }
+
+    int ans = 0;
+
+    for (int i = 0; i < B; i++) {
+        // cout << "i = " << i << ", dp = " << dp[N][i][1][1] << "\n";
+        ans = (ans + dp[N][i][1][1]) % MOD;
+    }
+
+    cout << ans;
+
+    return 0;
+}
