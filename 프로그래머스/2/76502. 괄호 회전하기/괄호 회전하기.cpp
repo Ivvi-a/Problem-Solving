5ed9a0d40f69c7de06aa4501b60d7bb8ea2db121
Yeonju Jo <53993041+Ivvi-a@users.noreply.github.com>
2024-12-03 16:35:52 +0900

    [level 2] Title: 괄호 회전하기, Time: 5.00 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    int len = s.length();
+    stack<char> st;
+    
+    for(int i=0; i<len; i++){
+        for(int k=0; k<len; k++){
+            char c = s.at((i+k)%len);
+            if(c == '(' || c == '[' || c == '{'){
+                st.push(c);
+            }
+            else if(c == ')'){
+                if(st.top() == '(') st.pop();
+                else break;
+            }
+            else if(c == ']'){
+                if(st.top() == '[') st.pop();
+                else break;
+            }
+            else if(c == '}'){
+                if(st.top() == '{') st.pop();
+                else break;
+            }
+            if(k == len-1 && st.size() == 0) answer++;
+        }
+    }
+    
+    return answer;
+}
