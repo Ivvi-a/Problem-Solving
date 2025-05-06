a6ddeea8619f245676faf44fe2c26e007187e742
Yeonju Jo <53993041+Ivvi-a@users.noreply.github.com>
2025-05-06 20:01:30 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int N, M;
+bool visited[9];
+vector<int> v;
+
+void recur(int depth) {
+	if (depth == M) {
+		for (int i = 0; i < M; i++) {
+			cout << v[i];
+			if (i != M - 1) cout << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 1; i <= N; i++) {
+		if (visited[i]) continue;
+		v.push_back(i);
+		visited[i] = true;
+		recur(depth + 1);
+		visited[i] = false;
+		v.erase(v.end() - 1);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> N; cin >> M;
+	recur(0);
+	return 0;
+} 
