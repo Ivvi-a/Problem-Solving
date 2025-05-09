7a4fe3315e2051f54362d0d12d01e849a262c2c9
Yeonju Jo <53993041+Ivvi-a@users.noreply.github.com>
2025-05-09 20:31:46 +0900

    [Silver III] Title: N과 M (5), Time: 24 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int N, M;
+bool visited[9];
+vector<int> arr;
+vector<int> seq;
+
+void recur(int depth) {
+	if (depth == M) {
+		for (int i = 0; i < M; i++) {
+			cout << seq[i];
+			if (i != M - 1) cout << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 0; i < N; i++) {
+		if (visited[i]) continue;
+		seq.push_back(arr[i]);
+		visited[i] = true;
+		recur(depth + 1);
+		visited[i] = false;
+		seq.erase(seq.end() - 1);
+	}
+}
+
+void init() {
+	for (int i = 0; i < N; i++) {
+		int n; cin >> n;
+		arr.push_back(n);
+	}
+	sort(arr.begin(), arr.end());
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> N; cin >> M;
+	init();
+	recur(0);
+	return 0;
+} 
