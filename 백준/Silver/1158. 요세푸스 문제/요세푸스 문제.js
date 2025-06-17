8805ba0772d1c5ead89d38d05caf9c06b118db98
Yeonju Jo <53993041+Ivvi-a@users.noreply.github.com>
2025-06-17 21:16:09 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 100 ms, Memory: 11056 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+
+const N = input[0];
+const K = input[1] - 1;
+
+let list = Array.from({ length: N }, (_, i) => i + 1);
+let idx = 0;
+
+let answer = "<";
+
+while (list.length > 0) {
+  idx = (idx + K) % list.length;
+  answer += list[idx];
+  if (list.length != 1) answer += ", ";
+  list.splice(idx, 1);
+}
+answer += ">";
+console.log(answer);
