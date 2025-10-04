02e97651ef4c06fe3ea16bc9cf0914370983c1c9
Yeonju Jo <53993041+Ivvi-a@users.noreply.github.com>
2025-10-04 23:59:25 +0900

    [Silver I] Title: 쿼드트리, Time: 16 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+bool isAllSame(vector<vector<int>> v, int x, int y, int len) {
+	for (int i = y; i < y + len; i++) {
+		for (int j = x; j < x + len; j++) {
+			if(v[y][x] != v[i][j]) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+string solve(string s, vector<vector<int>>& v, int x, int y, int len) {
+	// 1. 전체 색깔이 같은지 확인한다
+	if (isAllSame(v, x, y, len) == true) {
+		// 2-1. 같다면 string에 숫자를 저장
+		return to_string(v[y][x]);
+	}
+	else {
+		// 2-2. 다르다면 4등분해서 재귀
+		int half = len / 2;
+		string ss = "(";
+		ss += solve(s, v, x, y, half);
+		ss += solve(s, v, x + half, y, half);
+		ss += solve(s, v, x, y + half, half);
+		ss += solve(s, v, x+ half, y+ half, half);
+		ss += ")";
+		return ss;
+	}
+}
+
+vector<vector<int>> init() {
+	int N;
+	cin >> N;
+	vector<vector<int>> v(N, vector<int>(N));
+	for (int i = 0; i < N; i++) {
+		string s;
+		cin >> s;
+		for (int j = 0; j < N; j++) {
+			v[i][j] = s[j] - '0';
+		}
+	}
+	return v;
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	vector<vector<int>> v = init();
+	string answer = solve("", v, 0, 0, v.size());
+	cout << answer;
+	return 0;
+}
